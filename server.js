70b26f7366ddb3dcb0dfd71115676a061e1f4e1d
Michael <macherey@gmx.net>
2012-05-05 00:42:20 +0200

    setup

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Michael
+ * Date: 04.05.12
+ * Time: 22:23
+ * To change this template use File | Settings | File Templates.
+ */
+var http = require("http");
+
+function onRequest(request, response){
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log("Server has started.")
