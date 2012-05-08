1bac4c35593f28dc7a51e3ea95a3f2b2a8d15765
Michael <macherey@gmx.net>
2012-05-08 16:13:10 +0200

    added index JS, put server code into a function named start

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Michael
+ * Date: 08.05.12
+ * Time: 16:06
+ * To change this template use File | Settings | File Templates.
+ */
+var server = require("./server");
+server.start();
--- server.js
+++ server.js
@@ -7,14 +7,16 @@
  */
 "use strict";
 var http = require("http");
-
+function start(){
 function onRequest(request, response) {
     console.log("Request received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    response.write("Hello Worlds");
     response.end();
 }
 
 http.createServer(onRequest).listen(8888);
 
 console.log("Server has started.");
+}
+exports.start = start;
