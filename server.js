28b7b51cee351b52002ed107da7823795fa7f7ce
michael m <macherey@gmx.net>
2012-05-10 17:15:40 +0200

    Post data basics

--- server.js
+++ server.js
@@ -11,10 +11,17 @@ var url = require("url");
 function start(route, handle) {
     "use strict";
     function onRequest(request, response) {
+        var postData = " ";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
-
-        route(handle, pathname, response);
+        request.setEncoding("utf8");
+        request.addListener("data", function (postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '" + postDataChunk + "'.");
+        });
+        request.addListener("end", function () {
+            route(handle, pathname, response, postData);
+        });
     }
 
     http.createServer(onRequest).listen(8888);
